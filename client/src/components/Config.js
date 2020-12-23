699a37ec9653f1aea9dfadca56ab2acaf4b02a8b
hwb0218 <52212226+hwb0218@users.noreply.github.com>
2020-12-23 16:14:11 +0900

    Creating a landing page

---
+++ client/src/components/Config.js
@@ -0,0 +1,5 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = 'bb99a8dc854a066f2fb16fd2e86a445c';
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,43 @@
+import React, {useEffect, useState} from 'react'
+import { FaCode } from "react-icons/fa";
+import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
+import MainImage from "./Sections/MainImage";
+
+function LandingPage() {
+    const [movies, setMovies] = useState([]);
+    const [mainMovieImage, setMainMovieImage] = useState(null);
+    useEffect(() => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=ko&page=1`;
+        fetch(endpoint)
+            .then(response => response.json())
+            .then(response => {
+                console.log(response);
+                setMovies([response.results]);
+                setMainMovieImage(response.results[0]);
+            });
+    }, []);
+    return (
+        <div style={{ width: '100%', margin: '0' }}>
+            {/* Main Image */}
+            {mainMovieImage &&
+            <MainImage
+                image={`${IMAGE_BASE_URL}w1280${mainMovieImage.backdrop_path}`}
+                title={mainMovieImage.title}
+                text={mainMovieImage.overview}
+            />
+            }
+            <div style={{ width: '85%', margin: '1rem auto' }}>
+                <h2>Movies by latest</h2>
+                <hr/>
+
+                {/* Movie grid Cards */}
+            </div>
+
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <button>Load More</button>
+            </div>
+        </div>
+    )
+}
+
+export default LandingPage
