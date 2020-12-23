12114ab8e91071a0aad7b84a4d4e84e7484e8347
hwb0218 <52212226+hwb0218@users.noreply.github.com>
2020-12-23 18:22:33 +0900

    Grid card component

---
+++ client/src/components/views/commons/GridCards.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Col } from 'antd';
+
+const GridCards = ({image, movieId, movieName}) => {
+    return (
+        <Col lg={6} md={8} xs={24}>
+            <div style={{ position: 'relative' }}>
+                <a href={`/movie/${movieId}`}>
+                    <img style={{ width: '100%', height: '320px'}} src={image} alt={movieName}/>
+                </a>
+            </div>
+        </Col>
+    );
+};
+
+export default GridCards;
